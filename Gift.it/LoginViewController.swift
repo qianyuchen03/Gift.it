f87b8ff376745f4ab0c8d8b1fdee3b580576b8dd
Prerna Singh <singhprernap@utexas.edu>
2024-10-17 00:20:19 -0500

    login setup and splashscreen

---
+++ Gift.it/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  Gift.it
+//
+//  Created by Prerna Singh on 10/15/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        // Perform the segue to go to the next view controller
+        performSegue(withIdentifier: "LoginSegue", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Gift.it/MyTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MyTabBarController.swift
+//  Gift.it
+//
+//  Created by Prerna Singh on 10/17/24.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.selectedIndex = 2
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
